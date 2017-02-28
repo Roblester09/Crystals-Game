0cfcd098b7e83af4f5453c2ca7836f89216b1011
Robert Lester <Lesters.robert@gmail.com>
2017-02-28 18:25:28 -0500

    updated bug fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,9 @@ var score = 0;
 var wins = 0;
 var loss = 0;
 
-var audio_win = new Audio('stage_clear.wav');
-var coin = new Audio('coin.wav')
-var audio_loss = new Audio('mariodie.wav')
+var audio_win = new Audio("../assets/audio/stage_clear.wav");
+var coin = new Audio("../assets/audio/coin.wav");
+var audio_loss = new Audio("../assets/audio/mariodie.wav");
 
 var randomNum =  Math.floor(Math.random() * 31) + 50;
 
