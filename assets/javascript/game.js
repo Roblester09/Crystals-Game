4d7d75f75b6c979570229be2448f61f9ae89e87a
Robert Lester <Lesters.robert@gmail.com>
2017-02-28 20:17:53 -0500

    added wins and losses and fixed a couple bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,87 +1,91 @@
 
 $(document).ready(function(){
 
-var score = 0;
-var wins = 0;
-var loss = 0;
+    var score = 0;
+    var wins = 0;
+    var loss = 0;
 
-var audio_win = new Audio("../assets/audio/stage_clear.wav");
-var coin = new Audio("../assets/audio/coin.wav");
-var audio_loss = new Audio("../assets/audio/mariodie.wav");
+    var audio_win = new Audio("../assets/audio/stage_clear.wav");
+    var coin = new Audio("../assets/audio/coin.wav");
+    var audio_loss = new Audio("../assets/audio/mariodie.wav");
 
-var randomNum =  Math.floor(Math.random() * 31) + 50;
+    var randomNum =  Math.floor(Math.random() * 31) + 50;
 
-// Check if win or lose
-
-function check(){
-    if(score === randomNum){
-        audio_win.play();
-        alert("You win!");
-        wins++
-        $("#wins").html(wins);
-        resetScore();
-    } else if(score > randomNum){
-        audio_loss.play();
-        alert("You Lose!");
-        loss++;
-        $("#losses").html(loss);
-        resetScore();
-    }
-}
-
-// Reset Score
-
-function resetScore(){
-    score = 0;
+    $("#myScore").html(score);
     $("#randomNumber").html(randomNum);
-    $("#myScore").html(score);
-    update();
+    $("#wins").html(wins);
+    $("#losses").html(loss);
 
-    newNum();
-    $("#randomNumber").html(newNum);
-}
+    // Check if win or lose
+
+    function check(){
+        if(score === randomNum){
+            audio_win.play();
+            alert("You win!");
+            wins++
+            $("#wins").html(wins);
+            resetScore();
+        } else if(score > randomNum){
+            audio_loss.play();
+            alert("You Lose!");
+            loss++;
+            $("#losses").html(loss);
+            resetScore();
+        }
+    }
+
+    // Reset Score
+
+    function resetScore(){
+        score = 0;
+        randomNum =  Math.floor(Math.random() * 31) + 50;
+        $("#randomNumber").html(randomNum);
+        $("#myScore").html(score);
+    }
 
 
-// Update Score
+    // Update Score
 
-function update(){
-    $("#myScore").html(score);
-}
+    function update(){
+        $("#myScore").html(score);
+    }
 
-$("#randomNumber").html(randomNum);
+    function newNum(){
+        $("#randomNumber").html(randomNum);
+    }
 
-// Click Functionality
+    // Click Functionality
 
-$("#green-m").on("click", function(){
-    coin.play();
-    score ++;
-    update();
-    check();
-});
+    $("#green-m").on("click", function(){
+        coin.play();
+        score ++;
+        update();
+        check();
+    });
 
-$("#blue-m").on("click", function(){
-    coin.play();
-    score += 5;
-    update();
-    check();
-});
+    $("#blue-m").on("click", function(){
+        coin.play();
+        score += 5;
+        update();
+        check();
+    });
 
-$("#red-m").on("click", function(){
-    coin.play();
-    score += 2;
-    update();
-    check();
-});
+    $("#red-m").on("click", function(){
+        coin.play();
+        score += 2;
+        update();
+        check();
+    });
 
-$("#evil-m").on("click", function(){
-    coin.play();
-    score += 10;
-    update();
-    check();
-});
+    $("#evil-m").on("click", function(){
+        coin.play();
+        score += 10;
+        update();
+        check();
+    });
 
-//Reset
+    //Reset
 
-//End Page Load
+    //End Page Load
 
 });
